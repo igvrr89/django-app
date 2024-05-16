80d2524081823b2d43a46b8b89152d051ffed589
Igor4B <igorpaw47@gmail.com>
2024-05-16 08:33:16 +0200

    changes

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y0th)9h$(rjbukq#obl+_)@tf+3=*r!+$&4t706be5hi+n*xa8'
+SECRET_KEY = 'django-insecure-te2w!e8a0i)w^#^vwmpo5u65k4-$@4ix+vp*#nh$(!wnt7kp9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
